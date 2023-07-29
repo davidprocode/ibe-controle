b61bfd78ebd083f9207d74febaaadf61d03f99db
David Santos <davidprocode@proton.me>
2023-07-29 14:15:13 +0000

    okdok

--- src/Components/OnlineStudent/index.js
+++ src/Components/OnlineStudent/index.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 
-function calculateTimeLeft() {
-    return 1
+const handleStop = "Tempo esgotado!"
+
+const HandleTimeLeft = () => {
+    const [timeLeft, setTimeLeft] = useState(10)
+        useEffect(()=>{
+            setTimeout(()=>{
+                setTimeLeft(timeLeft > 0 ? timeLeft - 1 : timeLeft)
+            },1000)
+        },[timeLeft])
+    if (timeLeft === 0) {
+        console.log("Handle stop");
+    }                      
+    return <h1>{timeLeft > 0 ? timeLeft : handleStop}</h1>
 }
 
-export default () => {
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-    useEffect(() => {
-        setTimeout(()=>{
-            setTimeLeft(timeLeft => timeLeft + 1)
-        })
-    });      
-
-    return <div>
-        <h1>{timeLeft}</h1>
-    </div>
+const OnlineStudent = () => {
+    const name = "Aluno"
+    return <div>{name} - <HandleTimeLeft/></div>
 }
+
+export default OnlineStudent
